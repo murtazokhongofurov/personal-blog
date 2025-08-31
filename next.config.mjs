45a646bcd065c10c9d817ff1f21cf0917904aaa4
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-09-01 01:09:56 +0500

    feat: changed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  assetPrefix: '/',        // 🔑 Custom domain uchun
+  basePath: '',            // 🔑 Custom domain uchun
 }
+
 
 export default nextConfig
