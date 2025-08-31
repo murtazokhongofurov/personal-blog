a80b3409ace5e8029cdb7fec5d73754aadb96b4e
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-09-01 01:48:43 +0500

    feat: deleted out export folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 // next.config.mjs
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  assetPrefix: '/',        // 🔑 Custom domain uchun
-  basePath: '',            // 🔑 Custom domain uchun
 }
 
 
